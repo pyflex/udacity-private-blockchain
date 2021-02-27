3b3eb396d96c2b8f74a489a57fecc536ceb686fa
Alexander Törnvall <alextorn.business@gmail.com>
2021-02-27 16:15:52 +0100

    Fixed boilerplate bug - URL endpoints conflicting

--- BlockchainController.js
+++ BlockchainController.js
@@ -90,14 +90,15 @@ class BlockchainController {
 
   // This endpoint allows you to retrieve the block by hash (GET endpoint)
   getBlockByHash() {
-    this.app.get("/block/:hash", async (req, res) => {
+    // edited to fix bug with same endpoints, causing problems. Please change this.
+    this.app.get("/blockhash/:hash", async (req, res) => {
       if (req.params.hash) {
         const hash = req.params.hash.toString();
         let block = await this.blockchain.getBlockByHash(hash);
         if (block) {
           return res.status(200).json(block);
         } else {
-          return res.status(404).send("Block not found!");
+          return res.status(404).send("Block Not found!");
         }
       } else {
         return res.status(404).send("Block Not Found! Review the Parameters!");
